56913247a75a76e6549238d6c93f643b764fe161
Marc Fabre <nds.kram@gmail.com>
2022-02-25 10:54:28 +0100

    Corrected preview image

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -109,10 +109,18 @@ export default {
 
             return src;
         },
+        isPreviewImageWeWeb() {
+            return this.content.previewImage && this.content.previewImage.startsWith('designs/');
+        },
+        previewImageSrc() {
+            return this.isPreviewImageWeWeb
+                ? `${wwLib.wwUtils.getCdnPrefix()}${this.content.previewImage}`
+                : this.content.previewImage;
+        },
         videoAttributes() {
             const attributes = {
                 src: this.content.file,
-                poster: this.content.previewImage,
+                poster: this.previewImageSrc,
                 muted: true,
             };
 
@@ -124,6 +132,7 @@ export default {
 
             return attributes;
         },
+
         isEditing() {
             /* wwEditor:start */
             return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
